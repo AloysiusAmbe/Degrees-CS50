7021f2c6b8daffba1755cdf1662c4f35b2e3de02
Aloysius <ambealoys@gmail.com>
2020-11-24 00:56:43 -0500

    Can return the connection when there is only 1 degree of seperation

--- connect.py
+++ connect.py
@@ -4,6 +4,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from util import Node, Stack, Queue
+
 # DATABASE_URL=postgres://postgres:test@localhost:5432/degrees
 
 # Check for environment variable
@@ -14,12 +16,41 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-def main():
-    # visited = set()
-    # visited.add(102)
-    # neighbors = get_neighbors(102, visited)
 
-    get_person_id('Tom Hanks')
+def main():
+    path = find_connection(102, 158)
+    print(path)
+
+
+def find_connection(start_id, end_id):
+    '''
+    Finds and returns the connection between two actors
+    '''
+
+    # Keeps track of all ids which have been visited
+    visited = set()
+    visited.add(start_id)
+
+    # Gets all the stars who are in the same movie as the starting actor
+    neighbors = get_neighbors(start_id, visited)
+    queue = Queue()
+
+    for movie_id, star_id in neighbors:
+        # Checks if a connection has been found
+        if star_id == end_id:
+            path = {
+                'start': start_id,
+                'movie': movie_id,
+                'end': end_id
+            }
+            return path
+
+        # Stores each connection between actors
+        node = Node(start_id, movie_id, star_id)
+        queue.push(node)
+
+    while not queue.isEmpty():
+        path = []
 
 
 def get_neighbors(star_id: int, visited: set):
@@ -50,7 +81,7 @@ def get_person_id(star_name: str):
     Returns the IMDB id for a person's name,
     resolving ambiguities as needed.
     '''
-    rows = db.execute('SELECT star_name, star_id, birth FROM people WHERE star_name=:star_name',
+    rows = db.execute('SELECT star_id, birth FROM people WHERE star_name=:star_name',
     {
         'star_name': star_name
     })
@@ -58,10 +89,20 @@ def get_person_id(star_name: str):
     if rowcount == 0:
         return None
     elif rowcount > 1:
-        pass
+        star_ids = dict()
+        results = rows.fetchall()
+        for result in results:
+            star_id = result[0]
+            birth = result[1]
+            star_ids = {
+                'name': star_name,
+                'person_id': star_id,
+                'birth': birth
+            }
+        return star_ids
     else:
         star_id = rows.fetchall()
-        return star_id[0][1]
+        return star_id[0][0]
 
 
 if __name__ == "__main__":
