62f6e73c0208168e8beb5fd7f64ddfe9fa04510e
Aloysius <ambealoys@gmail.com>
2020-11-24 20:07:39 -0500

    Time wasted smh

--- connect.py
+++ connect.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from util import Node, Stack, Queue
 
-# DATABASE_URL=postgres://postgres:test@localhost:5432/degrees
+# set DATABASE_URL=postgres://postgres:test@localhost:5432/degrees
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -18,14 +18,12 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 def main():
-    path = find_connection(420, 398)
-    if path == None:
-        print(None)
-    else:
-        for i in path:
-            print(i.star_1_id, i.movie_id, i.star_2_id)
-        print()
-        pass
+    get_neighbors(102)
+    # path = find_connection(129, 102)
+    # if path == None:
+    #     print(None)
+    # else:
+    #     print(path)
 
 
 def find_connection(start_id, end_id):
@@ -40,25 +38,20 @@ def find_connection(start_id, end_id):
     # Gets all the stars who are in the same movie as the starting actor
     neighbors = get_neighbors(start_id, visited)
     queue = Queue()
-    paths = Queue()
+    paths = list()
 
     for movie_id, star_id in neighbors:
         # Checks if a connection has been found
         if star_id == end_id:
-            path_node = [Node(start_id, movie_id, star_id)]
-            return path_node
+            return [(movie_id, star_id)]
 
         # Stores each connection between actors
         node = Node(start_id, movie_id, star_id)
         queue.push(node)
 
-        list_node = list()
-        list_node.append(node)
-        paths.push(list_node)
+        paths.append([(movie_id, star_id)])
 
-    print(f'getting neighbors for {start_id}')
     print(neighbors)
-    print()
 
     while not queue.isEmpty():
         # Gets/pops the node values in the queue
@@ -69,22 +62,17 @@ def find_connection(start_id, end_id):
         # Gets the neighbors of the current node's second star
         neighbors = get_neighbors(current_star_id, visited)
 
-        print(f'getting neighbors for {current_star_id}')
-        print(neighbors)
-        print()
-
         for movie_id, star_id in neighbors:
-            print(f'{movie_id}: {star_id}')
             # Gets the current path
-            current_path = paths.pop()
-            node = Node(current_path[-1].star_2_id, movie_id, star_id)
-            current_path.append(node)
-            paths.push(current_path)
+            current_path = paths[0]
+            paths = paths[1:]
+            current_path.append((movie_id, star_id))
+            paths.append(current_path)
 
             # Checks if the connection has been found
             if star_id == end_id:
-                # Gets the lastest node added to the queue - the correct path
-                return paths.get_path()
+                # Gets the lastest path added to the list - the correct path
+                return paths[-1]
             
             node = Node(current_node.star_2_id, movie_id, star_id)
             queue.push(node)
@@ -93,26 +81,25 @@ def find_connection(start_id, end_id):
         return None # No path found
 
 
-def get_neighbors(star_id: int, visited: set):
+def get_neighbors(star_id: int):
     '''
     Gets all the stars who are in the same movie as the star passed into the function.
     '''
     neighbors = list()
-    movies = db.execute('SELECT movies.movie_id FROM movies JOIN stars ON movies.movie_id = stars.movie_id WHERE stars.star_id = :star_id',
+    rows = db.execute('SELECT movies, star_id FROM people WHERE star_id = :star_id',
             {
                 'star_id': star_id
             })
-    for movie_id in movies:
-        neighboring_stars = db.execute('SELECT people.star_id FROM people JOIN stars ON stars.star_id = people.star_id WHERE stars.movie_id = :movie_id',
-        {
-            'movie_id': movie_id[0]
-        })
 
-        for star in neighboring_stars:
-            star_id = star[0]
-            if star_id not in visited:
-                neighbors.append((movie_id[0], star_id))
-                visited.add(star_id)
+    for movie_ids, star_id in rows:
+        for movie_id in movie_ids.split():
+            stars = db.execute('SELECT stars FROM movies WHERE movie_id = :movie_id',
+                {
+                    'movie_id': movie_id
+                })
+            for star in stars:
+                for s in star:
+                    print(s)
     return neighbors
 
 
@@ -121,7 +108,7 @@ def get_person_id(star_name: str):
     Returns the IMDB id for a person's name,
     resolving ambiguities as needed.
     '''
-    rows = db.execute('SELECT star_id, birth FROM people WHERE star_name=:star_name',
+    rows = db.execute('SELECT star_id, birth FROM people WHERE star_name = :star_name',
     {
         'star_name': star_name
     })
@@ -130,15 +117,13 @@ def get_person_id(star_name: str):
         return None
     elif rowcount > 1:
         star_ids = dict()
-        results = rows.fetchall()
-        for result in results:
-            star_id = result[0]
-            birth = result[1]
-            star_ids = {
-                'name': star_name,
+        key = 0
+        for star_id, birth in rows.fetchall():
+            star_ids[key] = {
                 'person_id': star_id,
                 'birth': birth
             }
+            key += 1
         return star_ids
     else:
         star_id = rows.fetchall()
--- import.py
+++ import.py
@@ -15,46 +15,100 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, and a set of movie_ids
+people = {}
+
+# Maps movie_ids to a dictionary of title, year, and a set of person_ids
+movies = {}
+
+def load_data(path_name: str):
+    '''
+    Loads the data fron the CSV files into memory.
+    '''
+    # Loads movies
+    movies_reader = csv.reader(open(f'{path_name}/movies.csv', encoding='utf-8'))
+    for movie_id, title, year in movies_reader:
+        movies[movie_id] = {
+            'title': title,
+            'year': year,
+            'stars': set()
+        }
+
+    # Loads people
+    people_reader = csv.reader(open(f'{path_name}/people.csv', encoding='utf-8'))
+    for person_id, name, birth in people_reader:
+        people[person_id] = {
+            'name': name,
+            'birth': birth,
+            'movies': set()
+        }
+        if name.lower() not in names:
+            names[name.lower()] = {person_id}
+        else:
+            names[name.lower()].add(person_id)
+
+    # Loads stars
+    stars_reader = csv.reader(open(f'{path_name}/stars.csv', encoding='utf-8'))
+    for person_id, movie_id in stars_reader:
+        try:
+            people[person_id]['movies'].add(movie_id)
+            movies[movie_id]['stars'].add(person_id)
+        except:
+            pass
+
 def main():
     '''
     Imports the data to the database - movies, stars, and people
     '''
     path_name = 'small'
+    load_data(path_name)
+    print('Loaded')
 
     # Uploads stars data to db
-    # stars_reader = csv.reader(open(f'{path_name}/stars.csv', encoding='utf-8'))
-    # for star_id, movie_id in stars_reader:
-    #     db.execute('INSERT INTO stars (star_id, movie_id) VALUES (:star_id, :movie_id)',
-    #                 {
-    #                     'star_id': star_id,
-    #                     'movie_id': movie_id
-    #                 })
-    #     print(f'Added star_id: {star_id} and movie_id: {movie_id}')
-    #     db.commit()
+    stars_reader = csv.reader(open(f'{path_name}/stars.csv', encoding='utf-8'))
+    for star_id, movie_id in stars_reader:
+        db.execute('INSERT INTO stars (star_id, movie_id) VALUES (:star_id, :movie_id)',
+                    {
+                        'star_id': star_id,
+                        'movie_id': movie_id
+                    })
+    db.commit()
+    print('Loaded stars to db')
 
     # # Uploads people data to the db
-    # people_reader = csv.reader(open(f'{path_name}/people.csv', encoding='utf-8'))
-    # for star_id, name, birth in people_reader:
-    #     db.execute('INSERT INTO people (star_id, star_name, birth) VALUES (:star_id, :star_name, :birth)',
-    #             {
-    #                 'star_id': star_id,
-    #                 'star_name': name,
-    #                 'birth': birth
-    #             })
-    #     print(f'Added {name} to db.')
-    #     db.commit()
+    people_reader = csv.reader(open(f'{path_name}/people.csv', encoding='utf-8'))
+    for star_id, name, birth in people_reader:
+        movies_person_in = ''
+        for movie in people[star_id]['movies']:
+            movies_person_in += movie + ' '
+        db.execute('INSERT INTO people (star_id, star_name, movies, birth) VALUES (:star_id, :star_name, :movies, :birth)',
+                {
+                    'star_id': star_id,
+                    'star_name': name,
+                    'movies': movies_person_in,
+                    'birth': birth
+                })
+    db.commit()
+    print('Loaded people to db')
 
     # # Uploads the movies data to the db
-    # movies_reader = csv.reader(open(f'{path_name}/movies.csv', encoding='utf-8'))
-    # for movie_id, title, year in movies_reader:
-    #     db.execute('INSERT INTO movies (movie_id, movie_title, year) VALUES (:movie_id, :movie_title, :year)',
-    #             {
-    #                 'movie_id': movie_id,
-    #                 'movie_title': title,
-    #                 'year': year
-    #             })
-    #     print(f'Added {title} to db.')
-    #     db.commit()
+    movies_reader = csv.reader(open(f'{path_name}/movies.csv', encoding='utf-8'))
+    for movie_id, title, year in movies_reader:
+        stars_in_movie = ''
+        for person_id in movies[movie_id]['stars']:
+            stars_in_movie += person_id + ' '
+        db.execute('INSERT INTO movies (movie_id, movie_title, stars, year) VALUES (:movie_id, :movie_title, :stars, :year)',
+                {
+                    'movie_id': movie_id,
+                    'movie_title': title,
+                    'stars': stars_in_movie,
+                    'year': year
+                })
+    db.commit()
+    print('Loaded movies to db')
 
 
 if __name__ == "__main__":
